f168a75454b7410220053e4fd7687df53c2f8e0b
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-09-25 13:36:37 -0300

    Seleccionar noticias

--- bin/select_news.py
+++ bin/select_news.py
@@ -3,7 +3,7 @@ Mark news as selected=True
 """
 
 import fire
-from hatespeech_models import Article
+from hatespeech_models import Article, Reply
 from mongoengine import connect
 
 def select_articles(database, unselect_all_first=True):
@@ -16,24 +16,49 @@ def select_articles(database, unselect_all_first=True):
         Article.objects.update(selected=False)
 
     seeds = [
-        "China", "chino", "Cuba", "cubano", "bolivia",
-        "Trump", "judío", "camionero", "agresor",
-        "policía", "ladrón", "reprimir", "represión",
-        "juez", "justicia", "penal", "criminal",
-        "delito", "mamá", "género",
-        "aborto", "actriz", "feminista", "madre", "enfermera", "médica",
-        "piqueteros", "villa", "monos",
-        "Agustina", "Pichot",
-        "femicidios", "mujer", "trans",
-        "cristina", "macri", "morales", "canosa", "evo",
+        "China",
+        "Cuba",
+        "cubano",
+        "bolivia",
+        "Trump",
+        "judío",
+        "camionero",
+        "agresor",
+        #"policía",
+        "\"presos\"",
+        "ladrón",
+        "reprimir",
+        "represión",
+        #"juez",
+        #"justicia",
+        "penal",
+        "criminal",
+        "delito", "mamá",
+        "\"de género\"",
+        "\"aborto\"",
+        "\"actriz\"",
+        "feminista",
+        "piqueteros",
+        "villa",
+        "monos",
+        "\"Pichot\"",
+        "femicidio",
+        #"cristina",
+        #"macri",
+        "\"Evo Morales\"",
+        "\"canosa\"",
         "\"movimientos sociales\"",
-        "protesta",
+        "\"protesta\"",
+        "\"médica\"",
+        "\"Florencia Kirchner\"",
+        "\"enfermera\"",
+        "\"madre\"",
         "\"organizaciones sociales\"",
         "\"villa\"",
         "\"Nati Jota\"",
-        "\"Viale\"",
         "\"Ernestina Pais\"",
         "\"Mica Viciconte\"",
+        "\"Funes Ugarte\"",
         "\"la modelo\"",
         "\"la actriz\"",
         "\"la periodista\"",
@@ -45,26 +70,64 @@ def select_articles(database, unselect_all_first=True):
         "\"empleadas\"",
     ]
 
+
     seed_ids = set()
     seed_articles = []
+    print("Agregando por búsqueda en artículo")
 
-    queries = {}
 
     for word in seeds:
         query = Article.objects(dummy__ne=True, comments__19__exists=True).search_text(word)
         nuevos = 0
-        queries[word] = query
 
         for art in query:
             if art.id not in seed_ids:
                 nuevos += 1
                 seed_ids.add(art.id)
                 seed_articles.append(art)
-        print(f"{word} ---> {len(seed_articles)}, {nuevos} nuevos")
+        print(f"{word:<30} ---> {len(seed_articles):<5}, {nuevos:<4} nuevos")
 
-    print("Tenemos {len(seed_ids)}")
+    print("Seleccionando por comentarios")
+
+    comment_seeds = [
+        "judío",
+        "sionista",
+        "\"terrorista\"",
+        "monos",
+        "\"soros\"",
+        "\"puto\"",
+        "travesti",
+        "\"trolo\"",
+        "maricon",
+        "\"degenerado\"",
+        "\"trabuco\"",
+        "picaporte",
+    ]
+
+    for word in comment_seeds:
+        nuevos = 0
+
+        for rep in Reply.objects.search_text(word):
+            if len(rep.article.comments) < 20:
+                continue
+
+            art = rep.article
+
+            if art.id not in seed_ids:
+                nuevos += 1
+                seed_ids.add(art.id)
+                seed_articles.append(art)
+
+        print(f"{word:<30} ---> {len(seed_articles):<5}, {nuevos:<4} nuevos")
+
+
+    print(f"Tenemos {len(seed_ids)}")
+
+    distinct_titles = set(art.title for art in seed_articles)
+
+    print(f"Tenemos {len(distinct_titles)} títulos distintos")
     updated = Article.objects(id__in=seed_ids).update(set__selected=True)
-    print(f"Marcados como seleccionados {updated} artículo")
+    print(f"Marcados como seleccionados {updated} artículos")
 
 if __name__ == '__main__':
     fire.Fire(select_articles)
